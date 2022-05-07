a8dc96e837498730202930750b9440e82d5f2553
Ricardo Fabbri <rfabbri@gmail.com>
2022-05-07 19:05:42 -0300

    buff

--- p2pt/p2pt.hxx
+++ p2pt/p2pt.hxx
@@ -9,25 +9,20 @@
 #include "get_sigmas.hxx"
 #include "pose_from_point_tangents_2.hxx"
 
-
 namespace P2Pt {
   
 // This is the main routine
 template <typename T>
 void p2pt<T>::
 pose_from_point_tangents(
-	const T (&gama1)[3], const T (&tgt1)[3],
-	const T (&gama2)[3], const T (&tgt2)[3],
-	const T (&Gama1)[3], const T (&Tgt1)[3],
-	const T (&Gama2)[3], const T (&Tgt2)[3],
-	T (*output_RT)[RT_MAX_LEN][4][3], int *output_RT_len,
-	T *output_degen
+	const T (&gama1)[3], const T (&tgt1)[3], const T (&gama2)[3], const T (&tgt2)[3],
+	const T (&Gama1)[3], const T (&Tgt1)[3], const T (&Gama2)[3], const T (&Tgt2)[3],
+	T (*output_RT)[RT_MAX_LEN][4][3], int *output_RT_len, T *output_degen
 )
 {
 	T DGama[3] = { Gama1[0] - Gama2[0], Gama1[1] - Gama2[1], Gama1[2] - Gama2[2] };
   { // % test for geometric degeneracy -------------------------------
   const T norm = sqrt(DGama[0]*DGama[0] + DGama[1]*DGama[1] + DGama[2]*DGama[2]);
-  
 	// Matrix for degeneracy calculation
 	const T d[3][3] = {
 		DGama[0]/norm, Tgt1[0], Tgt2[0],
@@ -41,9 +36,7 @@ pose_from_point_tangents(
 	if (std::abs(degen) < 1.0e-3) {
     //TODO(openmvg) GUARD this print
 		std::cerr << "data point not reliable, please skip this solve within RANSAC" << std::endl;
-		output_RT     = nullptr;
-		output_RT_len = nullptr;
-		output_degen  = nullptr;
+		output_RT     = nullptr; output_RT_len = nullptr; output_degen  = nullptr;
 		return;
 	}
   }
@@ -58,7 +51,6 @@ pose_from_point_tangents(
 	// % compute rhos, r, t --------------------------
 	T rhos[3][ROOT_IDS_LEN];
 	int ts_len;
-
 	p.rhos_from_root_ids(root_ids, &rhos, &ts_len);
 
 	const T (&ts)[ROOT_IDS_LEN]    = rhos[0];
@@ -78,13 +70,10 @@ pose_from_point_tangents(
 	T (&RT)[RT_MAX_LEN][4][3] = *output_RT;
 	int &RT_len               = *output_RT_len;
 
-	p.get_r_t_from_rhos(
-		ts_len,
-		sigmas1, sigmas1_len, sigmas2, sigmas2_len,
-		rhos1, rhos2,
-		gama1, tgt1, gama2, tgt2,
-		Gama1, Tgt1, Gama2, Tgt2,
-		&RT, &RT_len);
+	p.get_r_t_from_rhos( ts_len, sigmas1, sigmas1_len, sigmas2, sigmas2_len,
+		rhos1, rhos2, gama1, tgt1, gama2, tgt2, Gama1, Tgt1, Gama2, Tgt2, 
+    &RT, &RT_len);
+  return true;
 }
 
 } // namespace p2pt
