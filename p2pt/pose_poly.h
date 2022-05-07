279d0cb83631e082e2c9f9c5d39549a64d8ec6bd
Ricardo Fabbri <rfabbri@gmail.com>
2022-05-07 19:10:05 -0300

    buff line cut

--- p2pt/pose_poly.h
+++ p2pt/pose_poly.h
@@ -28,18 +28,13 @@ struct pose_poly {
   // unsigned cnt_ = 0; // DEBUG iteration count
 
   static constexpr double T_LEN_2 = 2./T_LEN;
-  T t_vec(unsigned i) { return T_LEN_2*i -1; }
+  inline T t_vec(unsigned i) { return T_LEN_2*i -1.; }
 
 	void pose_from_point_tangents_2(
-		const T (&gama1)[3], const T (&tgt1)[3],
-		const T (&gama2)[3], const T (&tgt2)[3],
-		const T (&Gama1)[3], const T (&Tgt1)[3],
-		const T (&Gama2)[3], const T (&Tgt2)[3]
-	);
+		const T (&gama1)[3], const T (&tgt1)[3], const T (&gama2)[3], const T (&tgt2)[3],
+		const T (&Gama1)[3], const T (&Tgt1)[3], const T (&Gama2)[3], const T (&Tgt2)[3]);
   
-	inline void find_bounded_root_intervals(
-		T (*root_ids_output)[ROOT_IDS_LEN]
-	)
+	inline void find_bounded_root_intervals(T (*root_ids_output)[ROOT_IDS_LEN])
   {
     T curr_val = fn_t(t_vec(0)), next_val;
     for (unsigned i = 0; i < ROOT_IDS_LEN; i++) {
@@ -52,9 +47,8 @@ struct pose_poly {
   // o: output
 	inline T fn_t(const T t, T o[10]) 
   {
-    T &A = o[0]; T &B = o[1]; T &C = o[2]; T &E = o[3]; 
-    T &F = o[4]; T &G = o[5]; T &H = o[6]; T &J = o[7]; 
-    T &K = o[8]; T &L = o[9];
+    T &A = o[0]; T &B = o[1]; T &C = o[2]; T &E = o[3]; T &F = o[4]; 
+    T &G = o[5]; T &H = o[6]; T &J = o[7]; T &K = o[8]; T &L = o[9];
 
     //%function of t part :
     const T t2 = t*t, t3 = t2*t, t4 = t3*t, t5 = t4*t, t6 = t5*t, t7 = t6*t, t8 = t7*t;
@@ -74,13 +68,11 @@ struct pose_poly {
     K = (K0+K1*t+K2*t2+K3*t3-K2*t4+K1*t5-K0*t6)/t2p13;
     L = (L0+L1*t+L2*t2-L1*t3+L0*t4)/t2p12;
 
-    const T
-    A2=A*A, B2=B*B, C2=C*C, E2=E*E, F2=F*F, G2=G*G, H2=H*H, 
-    H3=H2*H, H4=H3*H, J2=J*J, J3=J2*J, K2=K*K, K3=K2*K, L2=L*L, L3=L2*L;
-
-    return
+    const T A2=A*A, B2=B*B, C2=C*C, E2=E*E, F2=F*F, G2=G*G, H2=H*H, 
+            H3=H2*H, H4=H3*H, J2=J*J, J3=J2*J, K2=K*K, K3=K2*K, L2=L*L, L3=L2*L;
+    
     // TODO: Analyze the need of extra/lower precison for the calculation of these terms.
-    E2*B2*H2*J2 +G2*C2*L*L*L*L +G2*A2*K3*K +E2*A2*H4 +E2*C2*J3*J
+    return E2*B2*H2*J2 +G2*C2*L*L*L*L +G2*A2*K3*K +E2*A2*H4 +E2*C2*J3*J
     +-2.*E*A*H2*G*C*L2 +2.*E2*A*H2*C*J2 +-2.*E2*C*J3*B*H +2.*E*C2*J2*G*L2
     +2.*E*A2*H2*G*K2 +-2.*E2*A*H3*B*J +-2.*E*A*H2*G*B*K*L +-2.*E*C*J2*G*B*K*L
     +-2.*E*C*J2*G*A*K2 +-2.*E*B*H*J*G*C*L2 +-2.*E*B*H*J*G*A*K2 +G2*B2*K2*L2
@@ -98,25 +90,19 @@ struct pose_poly {
 	inline T fn_t(const T t) { T b[10]; /*std::cerr << "eval count: " << ++cnt_ << std::endl;*/ return fn_t(t, b);  }
 	inline T operator()(T t) { return fn_t(t); }
   
-	void rhos_from_root_ids(
-		const T (&root_ids)[ROOT_IDS_LEN],
-		T (*output)[3][ROOT_IDS_LEN], int *output_ts_len
-	);
+	void rhos_from_root_ids(const T (&root_ids)[ROOT_IDS_LEN], T (*output)[3][ROOT_IDS_LEN], 
+      int *output_ts_len);
   
-	void get_sigmas(
-		const int ts_len, const T (&ts)[ROOT_IDS_LEN],
-		T (*output)[2][TS_MAX_LEN][TS_MAX_LEN], int (*output_len)[2][TS_MAX_LEN]
-	);
+	void get_sigmas(const int ts_len, const T (&ts)[ROOT_IDS_LEN], 
+      T (*output)[2][TS_MAX_LEN][TS_MAX_LEN], int (*output_len)[2][TS_MAX_LEN]);
   
 	void get_r_t_from_rhos(
 		const int ts_len,
 		const T (&sigmas1)[TS_MAX_LEN][TS_MAX_LEN], const int (&sigmas1_len)[TS_MAX_LEN],
 		const T (&sigmas2)[TS_MAX_LEN][TS_MAX_LEN], const int (&sigmas2_len)[TS_MAX_LEN],
 		const T (&rhos1)[ROOT_IDS_LEN], const T (&rhos2)[ROOT_IDS_LEN],
-		const T (&gama1)[3], const T (&tgt1)[3],
-		const T (&gama2)[3], const T (&tgt2)[3],
-		const T (&Gama1)[3], const T (&Tgt1)[3],
-		const T (&Gama2)[3], const T (&Tgt2)[3],
+		const T (&gama1)[3], const T (&tgt1)[3], const T (&gama2)[3], const T (&tgt2)[3],
+		const T (&Gama1)[3], const T (&Tgt1)[3], const T (&Gama2)[3], const T (&Tgt2)[3],
 		T (*output)[RT_MAX_LEN][4][3], int *output_len
 	);
 };
