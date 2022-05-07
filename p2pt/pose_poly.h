ce2a570f25ec93fd994667a6aecdbe5653f7fcb9
Ricardo Fabbri <rfabbri@gmail.com>
2022-05-07 19:25:33 -0300

    tight

--- p2pt/pose_poly.h
+++ p2pt/pose_poly.h
@@ -3,10 +3,9 @@
 
 namespace P2Pt {
 
-constexpr unsigned T_LEN = 2001;
-constexpr unsigned ROOT_IDS_LEN = T_LEN - 1;
+constexpr unsigned T_LEN = 2001, ROOT_IDS_LEN = T_LEN - 1;
 
-// TODO: Assume a reasonable length for `ts`. Check if it can be longer than 8.
+// At most 8 solutions with positive depth, TODO: assert if longer
 constexpr int TS_MAX_LEN = 8;
 constexpr int RT_MAX_LEN = (TS_MAX_LEN * TS_MAX_LEN);
 
@@ -16,8 +15,6 @@ struct pose_poly {
 		E0, E1, E2, F0, F1, F2, F3, G0, G1, G2, G3, G4,
 		H0, H1, H2, H3, H4, J0, J1, J2, J3, K0, K1, K2, K3,
 		L0, L1, L2, alpha, beta, theta, sth, cth;
-
-  // unsigned cnt_ = 0; // DEBUG iteration count
 
   static constexpr double T_LEN_2 = 2./T_LEN;
   inline T t_vec(unsigned i) { return T_LEN_2*i -1.; }
@@ -36,17 +33,13 @@ struct pose_poly {
     }
   }
   
-  // o: out
-	inline T fn_t(const T t, T o[10]) 
+	inline T fn_t(const T t, T o[10])  //o: output
   {
     T &A = o[0]; T &B = o[1]; T &C = o[2]; T &E = o[3]; T &F = o[4]; 
     T &G = o[5]; T &H = o[6]; T &J = o[7]; T &K = o[8]; T &L = o[9];
 
     //%function of t part :
     const T t2 = t*t, t3 = t2*t, t4 = t3*t, t5 = t4*t, t6 = t5*t, t7 = t6*t, t8 = t7*t;
-
-    // `(t^2 + 1)` integer powers
-    // TODO(check) when removing static, the tests accuse loss of precision
     const T t2p12 = (t2 + 1.) * (t2 + 1.), t2p13 = t2p12 * (t2 + 1.), t2p14 = t2p13 * (t2 + 1.);
 
     A = (A0+A1*t+A2*t2-A1*t3+A0*t4)/t2p12;
@@ -63,7 +56,6 @@ struct pose_poly {
     const T A2=A*A, B2=B*B, C2=C*C, E2=E*E, F2=F*F, G2=G*G, H2=H*H, 
             H3=H2*H, H4=H3*H, J2=J*J, J3=J2*J, K2=K*K, K3=K2*K, L2=L*L, L3=L2*L;
     
-    // TODO: Analyze the need of extra/lower precison for the calculation of these terms.
     return E2*B2*H2*J2 +G2*C2*L*L*L*L +G2*A2*K3*K +E2*A2*H4 +E2*C2*J3*J
     +-2.*E*A*H2*G*C*L2 +2.*E2*A*H2*C*J2 +-2.*E2*C*J3*B*H +2.*E*C2*J2*G*L2
     +2.*E*A2*H2*G*K2 +-2.*E2*A*H3*B*J +-2.*E*A*H2*G*B*K*L +-2.*E*C*J2*G*B*K*L
@@ -79,7 +71,7 @@ struct pose_poly {
     +-1.*F2*C*L2*B*H*J +G*E*B2*H2*L2 +G*E*B2*K2*J2 +8.*G*E*A*H*K*C*J*L;
   }
   
-	inline T fn_t(const T t) { T b[10]; /*std::cerr << "eval count: " << ++cnt_ << std::endl;*/ return fn_t(t, b);  }
+	inline T fn_t(const T t) { T b[10]; return fn_t(t, b);  }
 	inline T operator()(T t) { return fn_t(t); }
   
 	void rhos_from_root_ids(const T (&root_ids)[ROOT_IDS_LEN], T (*out)[3][ROOT_IDS_LEN], 
